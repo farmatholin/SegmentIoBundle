6fb5cd076424950724073fff0429fd9e9e8076d8
Vladislav <vladislav@forasoft.com>
2015-11-11 12:19:04 +0300

    add Provider template

---
+++ Util/SegmentIoProvider.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vladislav
+ * Date: 11.11.2015
+ * Time: 12:10
+ */
+
+namespace Farmatholin\SegmentIoBundle\Util;
+
+
+class SegmentIoProvider
+{
+    public function __construct()
+    {
+
+    }
+}
